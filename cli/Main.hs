b6cb873fc99e0c588d14b3329dc1189afcc1c4d6
jeigrei <j.g.cupit@gmail.com>
2019-05-17 16:38:58 -0500

    moving machines

--- cli/Main.hs
+++ cli/Main.hs
@@ -3,14 +3,24 @@
 module Main where
 
 import Options.Applicative
+import qualified System.Directory as SD
 
 import Lib
 import Files
 import CLI
 
+addFile :: String -> IO ()
+addFile name = return ()        -- TODO
+
+addToConfig :: FilePath -> HostedFileList -> IO ()
+addToConfig fpath hosted = return () -- TODO
+
+removeFile :: String -> IO ()
+removeFile name = putStrLn ("Removing the file: " ++ name)
+  
 main :: IO ()
 main = do
   opts <- execParser optsParser
   case optCommand opts of
-    Add name -> putStrLn("Adding the file: " ++ name)
-    Remove name -> putStrLn ("Removing the file: " ++ name)
+    Add name -> addFile name
+    Remove name -> removeFile name 
